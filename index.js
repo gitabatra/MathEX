8dae7ef261f3d08a55c8de3ff76313a3e8281c64
gitabatra <gitu20@gmail.com>
2023-02-28 16:26:58 -0330

    Add validation

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = {
         let page = document.body.id;
         switch(page) {
             case "student" :
-                //add js code for student file
+                //add js code for student
                 break;
             case "admin" :
                 //add js for admin
@@ -29,28 +29,39 @@ const app = {
 
     getNewTestInfo: () => {
 
-        document.getElementById("popupForm").addEventListener("submit", (e) => {
+        document.getElementById("newTestText").addEventListener("keyup",function (){
+            if(document.getElementById("newTestText").value.trim() != "") {
+                document.getElementById("addNewTestBtn").disabled = false;
+            } else {
+                document.getElementById("addNewTestBtn").disabled = true;
+            }
+       });
+
+      document.getElementById("popupForm").addEventListener("submit",function(e)
+      {  
+        if (!document.getElementById("popupForm").checkValidity()) {
             e.preventDefault();
-            const testName = document.getElementById("newTestText").value;
-            console.log(testName);
-    
-            const questionType =document.getElementById("selectQuestion");
-            var selectedQuestion = questionType.options[questionType.selectedIndex].text;
-         
-            console.log(selectedQuestion);
-    
-            const noOfDigits = document.getElementById("inputNumber").value;
-            console.log(noOfDigits);
-         
-            const firstNumber = document.getElementById("inputNumber1").value;
-            console.log(firstNumber);
-    
-            const secondNumber = document.getElementById("inputNumber2").value;
-            console.log(secondNumber);
-    
-            const ques = firstNumber + " + " + secondNumber + " ? ";
-            //document.getElementById("formData").innerHTML = ques;
-        })
+            e.stopPropagation();
+        }    
+        const testName = document.getElementById("newTestText").value;
+        console.log(testName);
+
+        const questionType =document.getElementById("selectQuestion");
+        var selectedQuestion = questionType.options[questionType.selectedIndex].text;
+        console.log(selectedQuestion);
+  
+        const noOfDigits = document.getElementById("inputNumber").value;
+        console.log(noOfDigits);
+       
+        const firstNumber = document.getElementById("inputNumber1").value;
+        console.log(firstNumber);
+      
+        const secondNumber = document.getElementById("inputNumber2").value;
+        console.log(secondNumber);
+        const ques = firstNumber + " + " + secondNumber + " = ? ";
+
+        document.getElementById("popupForm").classList.add('was-validated');
+      })
     }
 }
 
