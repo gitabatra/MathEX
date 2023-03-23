2ab45dd2b9b3fe11791e3fa92f1e75dd507c9db8
gitabatra <gitu20@gmail.com>
2023-03-23 21:12:14 -0230

    navigation link added and corresponding events added in events and index js files
    Events handled in events file.

--- events.js
+++ events.js
@@ -1,6 +1,34 @@
 function initEvents() {
+  
+ 
+  // $("link#navbar-student-btn").click(function () {
+  //   console.log("open Student Page navbar button event is executing");
+  //   window.location.href = "http://localhost:5500/index.html";
+  // });
+
+  // $("link#navbar-admin-btn").click(function () {
+  //   console.log("open Admin Page navbar button event is executing");
+  //   window.location.href = "http://localhost:5500/admin.html";
+  // });
+
   $("button#open-questionarie-btn").click(function () {
     console.log("open-questionarie button event is executing");
-    alert("Handler for .click() called.");
+    //alert("Handler for .click() called.");
+    window.location.replace("http://localhost:5500/studentQuestionary.html"); 
+  });
+
+  $("button#open-score-record-btn").click(function () {
+    console.log("open score record button event is executing");
+    window.location.href = "http://localhost:5500/score_record.html";
+  });
+
+  $("button#add-new-questionarie-Btn").click(function () {
+    console.log("Add new Questionarie button event is executing");
+    window.location.href = "http://localhost:5500/addNewTest.html";
+  });
+
+  $("button#open-edit-questionarie-btn").click(function () {
+    console.log("Edit Questionarie button event is executing");
+    window.location.href = "http://localhost:5500/addQuestions.html";
   });
 }
--- index.js
+++ index.js
@@ -3,3 +3,16 @@ $(document).ready(function () {
   console.log("Initializing Events");
   initEvents();
 });
+function onChange(obj){
+  var id= $(obj).attr("id");
+  switch(id){
+     case "0":
+      window.location.href = "http://localhost:5500/index.html";
+     break;
+
+     case "1":
+      window.location.href = "http://localhost:5500/admin.html";
+     break;
+
+  }
+}
